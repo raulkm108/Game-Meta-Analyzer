09d90aab437a6432d114e0f624523b1df7681439
raulkm108 <raulkm.civil@gmail.com>
2025-10-30 16:50:57 -0300

    Adding game mode counting

--- tests/Riot_API_test.py
+++ tests/Riot_API_test.py
@@ -45,21 +45,39 @@ user_matches_list = user_matches_info.json()
 
 wins = 0
 loses= 0
+arams = 0
+summoners_rifts = 0
+arenas = 0
 
 for match in user_matches_list:
     match_info_ulr = f"https://americas.api.riotgames.com/lol/match/v5/matches/{match}?api_key={api_key}"
     match_info = requests.get(match_info_ulr)
     player_index = match_info.json()["metadata"]["participants"].index(user_puuid)
     match_result = match_info.json()["info"]["participants"][player_index]["win"]
+    match_type = match_info.json()["info"]["gameMode"]
+    if match_type == "CHERRY":
+        match_type = "ARENA"
+        arenas += 1
+
+    if match_type == "CLASSIC":
+        match_type = "SUMMONER'S RIFT"
+        summoners_rift += 1
+
+    if match_type == "ARAM":
+        arams += 1
+
     if match_result:
         wins += 1
-        print(f"One Win ({wins})")
+        print(f"One Win ({wins} out of {wins + loses}) ({match_type})")
 
     else:
         loses += 1
-        print(f"One Lose ({loses})")
+        print(f"One Lose ({loses} out of {wins + loses}) ({match_type})")
 
-print(f"The summoner {summoner_name} has a winrate of {round(wins/(wins + loses), 2)} in the last {number_of_matches} games")
+print(f"""The summoner {summoner_name} has a winrate of {round(wins/(wins + loses), 2)} in the last {number_of_matches} games
+      {summoners_rifts} being Summoners Rift's
+      {arams} being Arams
+      {arenas} being Arenas""")
 
 tf = dt.datetime.now()
 
