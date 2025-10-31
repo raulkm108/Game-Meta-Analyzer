78858fd3a168a727ec427186f599b44e9ff14249
raulkm108 <raulkm.civil@gmail.com>
2025-10-31 14:03:06 -0300

    Adding new api key

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from src.data.api_client import RiotAPIClient
 
-API_KEY = 'RGAPI-3e2219b5-edf0-4e48-99f4-971e4fe0e5d2'
+API_KEY = 'RGAPI-9d00f3d3-5fcf-4121-9a53-08e917205890'
 
-request = RiotAPIClient(api_key=API_KEY,region="americas" )
+request = RiotAPIClient(api_key=API_KEY,region="americas")
