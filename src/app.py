38b2041547a94a8d23c390cc7f294c5ed2762d9d
raulkm108 <raulkm.civil@gmail.com>
2025-10-31 14:57:28 -0300

    Implementing app.py
    Implemeting and instancing simple functions to actual test the methods

--- src/app.py
+++ src/app.py
@@ -1,5 +1,22 @@
-from src.data.api_client import RiotAPIClient
+from data.api_client import RiotAPIClient
+from dotenv import load_dotenv
+import os
 
-API_KEY = 'RGAPI-9d00f3d3-5fcf-4121-9a53-08e917205890'
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
 
 request = RiotAPIClient(api_key=API_KEY,region="americas")
+
+user_puuid = request.acquire_puuid(summoner_name="SnowTime", summoner_tag="br1")
+
+print(user_puuid)
+
+number_of_matches = 10
+user_match_list = request.acquire_match_list(user_puuid=user_puuid, number_of_matches=number_of_matches)
+
+print(user_match_list)
+
+user_stats = request.get_user_stats(user_puuid=user_puuid, user_matches_list=user_match_list)
+
+print(user_stats)
